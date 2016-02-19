c09f92e2de4039e0ace228200a931420dfb1d3f9
asahinashuji <box6409@gmail.com>
2016-02-19 10:06:07 +0000

    micropost-heroku

--- db/migrate/20160214121747_add_micropost_id_to_microposts.rb
+++ db/migrate/20160214121747_add_micropost_id_to_microposts.rb
@@ -1,5 +1,5 @@
 class AddMicropostIdToMicroposts < ActiveRecord::Migration
   def change
-    add_column :microposts, :micropost_id, :integere
+    add_column :microposts, :micropost_id, :integer
   end
 end
